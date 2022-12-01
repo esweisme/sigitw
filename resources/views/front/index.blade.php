f2c1aa7888df83513498a9cd129c222beb5723b3
Sigit Wahyudi <wahyudi.sigit@gmail.com>
2022-12-01 15:12:08 +0800

    font

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,11 +3,22 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://eswe.netlify.app/images/logo.JPG" rel="icon">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cherry+Swash&family=Quattrocento+Sans&display=swap" rel="stylesheet">
     <title>Sigit Wahyudi TIK UBT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" href="/css/style.css" >
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container-lg">
+        <h1>Hello, world!</h1>
+        <h3>
+            under construction
+            <small class="text-muted"><a href="mailto:wahyudi.sigit@gmail.com">eSWe</a></small>
+        </h3>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
