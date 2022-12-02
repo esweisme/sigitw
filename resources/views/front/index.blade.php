e58c85b35485f663ac5a759036949881179644db
Sigit Wahyudi <wahyudi.sigit@gmail>
2022-12-02 11:15:07 +0800

    front

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,21 +3,41 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://eswe.netlify.app/images/logo.JPG" rel="icon">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="/images/logo-eswe.jpg" rel="icon">
+	  <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cherry+Swash&family=Quattrocento+Sans&display=swap" rel="stylesheet">
     <title>Sigit Wahyudi TIK UBT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	<link rel="stylesheet" href="/css/style.css" >
+	  <link rel="stylesheet" href="/css/style.css" >
   </head>
   <body>
-    <div class="container-lg">
-        <h1>Hello, world!</h1>
-        <h3>
-            under construction
-            <small class="text-muted"><a href="mailto:wahyudi.sigit@gmail.com">eSWe</a></small>
-        </h3>
+    <div class="container">
+      <header class="text-center mt-2">
+          <h1>Sigit Wahyudi</h1>
+          <h3>Kang Koding Utara Indonesia</h3>
+      </header>
+      <nav>
+
+      </nav>
+      <main>
+        <div class="tengah">
+          
+          <div class="b">
+            <div class="d-flex justify-content-center text-primary">
+              <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+            <p class="text-center text-primary">under construction</p>
+          </div>
+        </div>
+      
+
+      </main>
+      <footer>
+          <p class="copyright">&#169; 2022 by eswe with ❤ & ☕</p>
+      </footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
