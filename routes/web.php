450a59ba5cc7965f1c0c8984e1873ecce337ee12
Sigit Wahyudi <wahyudi.sigit@gmail>
2024-02-27 11:54:30 +0800

    s

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
+use App\Http\Controllers\toolController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use App\Http\Controllers\FrontController;
 
 
 Route::get('/', [FrontController::class, 'index']);
+
+Route::get('/tools/wppost', [toolController::class, 'removeScript']);
